eae2adcdcd25f6be84e9f7e74b8d30f7b7b8fa38
regevemous <regevemous@gmail.com>
2016-05-25 14:04:27 +0300

    Add files via upload

--- Hackaton/js/game.js
+++ Hackaton/js/game.js
@@ -1,6 +1,9 @@
-var questionData = [{question:"Where is our university located?",answer:"op3", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"}];
-			   
+
 function LoadQuestion() {
+    var questionData =   [{question:"Where is our university located?",answer:"op3", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"}];
+var q="Where is our university located?";
+var answer=[];
+var index=0;
     $("question-area").empty(); 
 	$("input1").empty(); 
 	$("input2").empty(); 
@@ -14,23 +17,26 @@ function LoadQuestion() {
 		index=0;
     } else {
         dataArray = JSON.parse(localStorage.dataArray);
-		index= JSON.parse(localStorage.dataArray);
+		//index= JSON.parse(localStorage.dataArray);
     }
     
-        var trHtml = "";
-        trHtml =data[index].question;
-        $("#question-area").value=trHtml);
-		$("input1").value= data[index].op1;
-		$("input2").value= data[index].op2;
-		$("input3").value= data[index].op3;
-		$("input4").value= data[index].op4;
+       
+        $("#question-area").text(q);
+		$("#input1").val(questionData[index].op1);
+		$("#input2").val(questionData[index].op2);
+		$("#input3").val(questionData[index].op3);
+		$("#input4").val(questionData[index].op4);
+
 }
 
-function saveQuestion(var answer) {
-    var store = {};
+function saveQuestion(answerUser) {
+    var userAnswer = {};
 
-    store.dat =answer;
+    userAnswer.answer =answerUser;
+    userAnswer.index= 0;
 
+    
+    
     
     // init / load current array
     var dataArray;
@@ -41,8 +47,9 @@ function saveQuestion(var answer) {
     }
     
     // add new person to current array
-    dataArray.push(store);
+    dataArray.push(userAnswer);
     
     // save to storage
     localStorage.dataArray = JSON.stringify(dataArray);
+    LoadQuestion();
 }
