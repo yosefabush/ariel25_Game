1e2c5c84fe45139af64b515448abe5efcb5f8657
regevemous <regevemous@gmail.com>
2016-05-25 14:34:41 +0300

    Update game.js

--- Hackaton/js/game.js
+++ Hackaton/js/game.js
@@ -1,5 +1,6 @@
-
+function onload()
 function LoadQuestion() {
+    var flag=false;
     var questionData =   [{question:"Where is our university located?",answer:"op3", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"}];
 var q="Where is our university located?";
 var answer=[];
@@ -21,19 +22,34 @@ var index=0;
     }
     
        
-        $("#question-area").text(q);
+        $("#question-area").text(dataArray[index].question);
 		$("#input1").val(questionData[index].op1);
 		$("#input2").val(questionData[index].op2);
 		$("#input3").val(questionData[index].op3);
 		$("#input4").val(questionData[index].op4);
+        setInterval(checkServer(), 1000);
+    function checkServer(){
+    var answer=JSON.parse(localStorage.dataArray);
+    if(answer=true)
+        clearInterval(0);
+        
+            
 
 }
 
 function saveQuestion(answerUser) {
+        var questionData =   [{question:"Where is our university located?",answer:"op3", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"}];
+var q="Where is our university located?";
+var answer=[];
+var index=0;
     var userAnswer = {};
 
     userAnswer.answer =answerUser;
     userAnswer.index= 0;
+    if(answerUser.equals(questionData[index].answer))
+        localStorage.dataArray = JSON.stringify(true);
+    else
+        localStorage.dataArray = JSON.stringify(false);
 
     
     
@@ -51,5 +67,8 @@ function saveQuestion(answerUser) {
     
     // save to storage
     localStorage.dataArray = JSON.stringify(dataArray);
-    LoadQuestion();
+
+}
+
+    
 }
