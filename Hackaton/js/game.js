654ef4b8a5e4beb576711c96c657454398955848
Asaf.Y <asafyeshayahu@gmail.com>
2016-05-26 14:54:28 +0200

    changed functions to GET. also changed DB access so it will get both gameId and Status instead of seperate functions

--- Hackaton/js/game.js
+++ Hackaton/js/game.js
@@ -1,15 +1,11 @@
 
-  var dataQuestion; 
+var dataQuestion; 
 var index;//index of question in dataQuestion
 var flag;//interval stopper
-var gameId=1; // for testing purposes
+var gameId=1; // for testing purposes (PIN GAME will be updated here)
 
 
 function LoadQuestion() {
-    
-
-
-
     $("question-area").empty(); 
 	$("input1").empty(); 
 	$("input2").empty(); 
@@ -24,52 +20,42 @@ function LoadQuestion() {
 		$("#input2").val(dataQuestion[index].Answer2);
 		$("#input3").val(dataQuestion[index].Answer3);
 		$("#input4").val(dataQuestion[index].Answer4);
-
-
-            
-
+		
+    flag=setInterval(checkServer(), 5000);
 }
+
 function checkServer(){
-   $.ajax({
-       type:"POST",
-       url: "getCurrentQuestion.php",
-       data:{dataQuestion.GameId},
-      success:function(data){
-       if(data!=dataQuestion[index].Id)
+
+	// Check with the server wheter to advance to the next question
+     dataQuestion = $.ajax("getGameInfo.php?req=getGameInfo&gameId=" + gameId).done(function(data) {
+	  gameId = data.GameId; 
+	  if(data!=dataQuestion[index].Id)
        {
-       flag=null;
-       if(data.Status==0){
-       index++;
-       LoadQuestion();
-   }
-       else
-       window.location("winner.html");
-}
-   
-}
-   }
-       
-   }
-       
-       
-   }
-       
-   });
-    
+		   flag=null;
+		   if(data.Status==0){
+		   index++;
+		   LoadQuestion();
+			}
+		   else
+		   window.location("winner.html");
+		}
+    }).fail(function(data) {
+            alert("fail:" + data);
+    }).always(function(data) {
+            alert("always:" + data);
+    });
 }
 
 function saveQuestion(answerUser) {
-    var userAnswer = {};
 
-    userAnswer.answer =answerUser;
-    
-
-           flag=setInterval(checkServer(), 1000);
-
-
-    
-    // save to storage
-
+		// updaing the database (inserting and 
+	  var result = $.ajax("setAnswer.php?req=getQuestions&gameId=" + gameId).done(function(data) {
+            alert("good:" + data);
+    }).fail(function(data) {
+            alert("fail:" + data);
+    }).always(function(data) {
+            alert("always:" + data);
+    });
 }
 
     
@@ -79,8 +65,6 @@ $(document).ready(function() {
         
 function onLoading()
 {
-   
-   
    dataQuestion = $.ajax("getQuestions.php?req=getQuestions&gameId=" + gameId).done(function(data) {
             alert("good:" + data);
     }).fail(function(data) {
@@ -89,6 +73,7 @@ function onLoading()
             alert("always:" + data);
     });
      
+	 // TODO: function to get the initial value of gameId
 
 
       index=dataQuestion[0];  
