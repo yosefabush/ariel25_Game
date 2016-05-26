fa71aa3cad8483c9d3efce83da5d5d6410819f90
regevemous <regevemous@gmail.com>
2016-05-26 10:15:55 +0300

    Update game.js

--- Hackaton/js/game.js
+++ Hackaton/js/game.js
@@ -6,6 +6,91 @@ function LoadQuestion() {
 
 var q="Where is our university located?";
 var answer=[];
+a var firsttime=0;  
+var index=0;
+var ans;
+function LoadQuestion() {
+    var flag=false;
+
+
+var answer=[];
+
+    $("question-area").empty(); 
+	$("input1").empty(); 
+	$("input2").empty(); 
+	$("input3").empty(); 
+	$("input4").empty(); 
+    
+    var dat;
+
+    if (localStorage.dataArray === undefined) {
+        dat = [];
+		
+    } else {
+        
+        dat = JSON.parse(localStorage.dataArray); 
+        index++;
+    }
+    
+       var trHtml = "";
+        trHtml = "<h>" + dat[index].question+ "</h>";
+        
+    $("#question-area").empty();
+    $("#question-area").append(trHtml);
+		$("#input1").val(dat[index].op1);
+		$("#input2").val(dat[index].op2);
+		$("#input3").val(dat[index].op3);
+		$("#input4").val(dat[index].op4);
+       // setTimeout(checkServer(), 1000);
+
+            
+
+}
+
+function saveQuestion(answerUser) {
+    var userAnswer = {};
+
+    userAnswer.answer =answerUser;
+    
+    if(answerUser===dat[index].answer)
+        localStorage.answer = JSON.stringify(true);
+    else
+        localStorage.answer = JSON.stringify(false);
+    
+
+   
+    
+    
+
+    
+    // save to storage
+
+    onload();
+
+}
+
+        
+function onLoading()
+{
+    var data = [{question:"Where is our university located?",answer:"Ariel", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"},
+    {question:"Where is our university 1?",answer:"Ariel", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"},
+    {question:"Where is our university 2?",answer:"Ariel", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"},
+    {question:"Where is our university 3?",answer:"Ariel", op1:"Tel-Aviv", op2:"Haifa", op3:"Ariel", op4: "Eilat"}];
+
+    
+
+if(firsttime==0){
+    localStorage.dataArray = JSON.stringify(data);
+
+    firsttime=1;
+LoadQuestion();
+
+}
+    LoadQuestion();
+}   
+
+
+    
 
     $("question-area").empty(); 
 	$("input1").empty(); 
