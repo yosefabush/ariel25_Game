3505af1e47bde9c5b9aa233481facb202cd5667f
Asaf.Y <asafyeshayahu@gmail.com>
2016-05-26 16:06:19 +0200

    added game timer, for game score

--- Hackaton/js/game.js
+++ Hackaton/js/game.js
@@ -2,8 +2,19 @@ var dataQuestion;
 var index;//index of question in dataQuestion
 var flag;//interval stopper
 var gameId=1; // for testing purposes (PIN GAME will be updated here)
-var seconds = 0;
+var seconds = 0.00; //timing of answer (timestamp)
+var timer; // hold the timer function
+var user;
 
+
+// to hold the user's information from login screen (Constructor for USER)
+function User(UserId, QuestionId, Answer, Time, IsCorrectAnswer) { 	
+	this.UserId = UserId;
+	this.QuestionId = QuestionId;
+	this.Answer = Answer;
+	this.Time = Time;
+	this.IsCorrectAnswer = IsCorrectAnswer;
+}
 
 function LoadQuestion() {
     $("question-area").empty(); 
@@ -32,11 +43,12 @@ function checkServer(){
 	  if(data!=dataQuestion[index].Id)
        {
 		   flag=null;
-		   if(data.Status==0){
+		   if(data.Status==0){ //game is active
 		   index++;
+		   timer = null;
 		   LoadQuestion();
 			}
-		   else
+		   else // need to check if won or not
 		   window.location("winner.html");
 		}
     }).fail(function(data) {
@@ -48,17 +60,19 @@ function checkServer(){
 
 function setAnswer(answerUser) {
 
-		// updaing the database (inserting and 
-	  var result = $.ajax("setAnswer.php?req=setAnswer&gameId=" + gameId).done(function(data) {
-	  /*
-	  * UserId
-	  * QuestionId
-	  * Answer
-	  * Time
-	  * IsCorrectAnswer
-	  */
+	// updaing the database (insert and update)
+	  var result = $.ajax("setAnswer.php?req=setAnswer" + 
+	  "&UserId=" + user.UserId + 
+	  "&QuestionId=" + user.QuestionId + 
+	  "&Answer=" + user.Answer + 
+	  "&Time=" + user.Time + 
+	  "&IsCorrectAnswer=" + user.IsCorrectAnswer).done(function(data) {
+	  
+	  
     }).fail(function(data) {
-            
+	
+	//debug purposes
+       alert("fail");
     });
 }
 
@@ -69,6 +83,13 @@ $(document).ready(function() {
         
 function onLoading()
 {
+	//set timer
+	timer = setInterval(setSeconds(), 100);
+	
+	//TODO: Need function here to load the information from the login screen
+	user = new User(1, 1, 1, 0, false);
+	
+	// Populate the page with questions
    dataQuestion = $.ajax("getQuestions.php?req=getQuestions&gameId=" + gameId).done(function(data) {
             alert("good:" + data);
     }).fail(function(data) {
@@ -78,10 +99,14 @@ function onLoading()
     });
      
 	 // TODO: function to get the initial value of gameId
+	
+	
+    index=0;  
 
+    LoadQuestion();
+}
 
-      index=dataQuestion[0];  
-
-    LoadQuestion();    
-
+// timer functions
+function setSeconds(){
+	seconds += 0.01;
 }
