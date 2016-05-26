01ab12d41004295e4be2d92ee21f7f190cde04d1
anael92 <anaelshomrai@gmail.com>
2016-05-26 15:37:40 +0300

    get Current Question added

---
+++ Hackaton/getCurrentQuestion.php
@@ -0,0 +1,17 @@
+<?php
+/*
+This file send all the data from questions table (question text,image,video url,answer1,answer2,answer3,answer4,correct answer number)
+*/
+    try{
+        require_once ('db_connection.php');	
+
+        if (isset($_GET['req']) && $_GET['req'] == "getCurrentQuestion"
+           && isset($_GET['gameId'])) {
+            $result = $db->query("SELECT * FROM games WHERE Id=".$_GET['gameId'])->fetchAll(PDO::FETCH_ASSOC);
+            echo(json_encode($result));
+        } 
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    } 
+?>
