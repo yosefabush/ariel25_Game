6304d51f3580f15066dc19740672d9fe65f80b73
anael92 <anaelshomrai@gmail.com>
2016-06-16 22:04:37 +0300

    addition of finishGame and setStatus
    finishGame - set the game status to 0
    setStatus - set the game status to 1

---
+++ Hackaton/page1/finishGame.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+/*
+Logically should be in Administraion 
+This file sets the status of the game to 1 (online)
+*/
+    try{
+        
+        
+        require_once ('../db_connection.php');	
+        $id = $_POST["gameId"];
+		
+		$res = $db->exec("UPDATE games SET Status = 0, Question = 0 WHERE Id = $id");
+		
+		if(  $res !== FALSE ) {
+            echo 1;   
+        } else {
+            echo -1;
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    } 
+?>
